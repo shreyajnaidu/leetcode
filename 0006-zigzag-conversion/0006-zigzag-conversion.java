45b2744c06d2ab83a3a245904585efff9ee8a730
shreyajnaidu <shreyajnaidu8@gmail.com>
2026-07-07 21:20:12 +0530

    Time: 5 ms (55.04%), Space: 46.5 MB (63.82%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,60 +1,38 @@
 class Solution {
-
     public String convert(String s, int numRows) {
 
         if (numRows == 1 || s.length() <= numRows) {
             return s;
         }
 
-        List<char[]> zigzag = new ArrayList<>();
+        List<StringBuilder> rows = new ArrayList<>();
 
-        int i = 0;
-
-        while (i < s.length()) {
-
-            // Downward column
-            char[] arr = new char[numRows];
-
-            int row = 0;
-
-            while (row < numRows && i < s.length()) {
-                arr[row] = s.charAt(i);
-                row++;
-                i++;
-            }
-
-            zigzag.add(arr);
-
-            // Upward diagonal
-            row = numRows - 2;
-
-            while (row > 0 && i < s.length()) {
-
-                char[] diag = new char[numRows];
-
-                diag[row] = s.charAt(i);
-
-                zigzag.add(diag);
-
-                row--;
-                i++;
-            }
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
         }
 
-        // Read row by row
-        StringBuilder ans = new StringBuilder();
+        int currentRow = 0;
+        int direction = 1;
 
-        for (int r = 0; r < numRows; r++) {
+        for (char ch : s.toCharArray()) {
 
-            for (char[] arr : zigzag) {
+            rows.get(currentRow).append(ch);
 
-                if (arr[r] != '\0') {
-                    ans.append(arr[r]);
-                }
-
+            if (currentRow == 0) {
+                direction = 1;
+            } else if (currentRow == numRows - 1) {
+                direction = -1;
             }
+
+            currentRow += direction;
         }
 
-        return ans.toString();
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
 }
