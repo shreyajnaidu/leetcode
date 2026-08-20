e97f3c81be80bc92c2e6add7d61c9c7bb9c81be8
shreyajnaidu <shreyajnaidu8@gmail.com>
2026-08-20 21:55:22 +0530

    Time: 3 ms (99.32%), Space: 48.4 MB (69.59%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        Deque<Integer> st=new ArrayDeque();
-        int n=nums.length;
-        for(int i=0;i<2*n;i++){
-            int index=i%n;
-            int current=nums[index];
-            while(!st.isEmpty() && current>nums[st.peek()]){
-                int x=st.pop();
-                map.put(x,current);
-            }
+    int n = nums.length;
+    int[] ans = new int[n];
+    Arrays.fill(ans, -1);
+
+    Deque<Integer> st = new ArrayDeque<>();
+
+    for (int i = 0; i < 2 * n; i++) {
+        int index = i % n;
+        int current = nums[index];
+
+        while (!st.isEmpty() && current > nums[st.peek()]) {
+            int x = st.pop();
+            ans[x] = current;
+        }
+
+        if (i < n) {
             st.push(index);
         }
-        int[] arr=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(i)){
-                arr[i]=map.get(i);
-            }else{
-                arr[i]=-1;
-            }
-        }
-        return arr;
     }
+
+    return ans;
+}
 }
